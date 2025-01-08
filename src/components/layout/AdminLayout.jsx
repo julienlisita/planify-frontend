394a05aef8dfdb093f205a11624c1aff4cd2b335
Julien Lisita <julien.lisita@gmail.com>
2025-01-08 16:05:52 +0100

    add structure for style files

--- src/components/layout/AdminLayout.jsx
+++ src/components/layout/AdminLayout.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import AdminNavbar from '../admin/AdminNavbar';
 import AdminSidebar from '../admin/AdminSidebar';
 
 const AdminLayout = ({ children }) => {
