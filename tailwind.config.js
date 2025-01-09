b55d1ac4f9a489ad9c7981124e64ad63a99b2677
Julien Lisita <julien.lisita@gmail.com>
2025-01-10 00:23:37 +0100

    create postcss.config.js files + Add Tailwind CSS directives in globals.css + add colors in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        taupe: '#453F3C',         // Couleur taupe sombre
+        linen: '#F7EDE2',         // Couleur lin (beige clair)
+        mossGreen: '#8A9A5B',     // Vert mousse
+        brownSugar: '#C76A4A',    // Brun "brown sugar" (orange brun)
+        black: '#000000',         // Noir
+      }
+    },
   },
   plugins: [],
 }
