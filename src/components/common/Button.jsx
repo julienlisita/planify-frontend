cbecda22b4ce0c481eeaf2e6cf68f881e9994cbe
Jean-Jacques Gilardi <jeanjacques.gilardi@gmail.com>
2025-01-13 19:13:36 +0100

    The Button and Input component have been created

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React from 'react'
 
-const Button = () => {
-  return <button>Button</button>;
+const Button = ({ value, onClick }) => {
+  return (<div>
+    <button
+      className="bg-taupe text-linen m-2 py-2 px-4 rounded"
+      type='button'
+      onClick={() => onClick(value)} >
+      {value}
+    </button>
+  </div>
+  );
 };
 
-export default Button;
+export default Button
--- src/components/common/Input.jsx
+++ src/components/common/Input.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
-const Input = () => {
-  return <div>Input</div>;
-};
+const Input = ({placeholder, value, onChange, label}) => {
+  return <div>
+    <label className="block text-taupe text-sm font-bold mb-2" htmlFor="input-id">{label}</label>
+    <input
+      id="input-id"
+      type="text"
+      className="text-taupe text-sm font-bold mb-2"
+      value={value}
+      placeholder={placeholder}
+      onChange={(e) => onChange(e.target.value)}
+    />
+  </div>
+}
 
 export default Input;
