f45ed0b9f73f942cb3486ee1cdfac0290e3f94c6
Julien Lisita <julien.lisita@gmail.com>
2025-01-14 16:48:14 +0100

    feat(button): add className prop for custom styling override

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const Button = ({ value, onClick }) => {
+const Button = ({ children, onClick , className }) => {
   return (<div>
     <button
-      className="bg-taupe text-linen m-2 py-2 px-4 rounded"
+      className={`bg-taupe text-linen hover:bg-mossGreen py-2 px-4 rounded-full ${className}`}
       type='submit'
       onClick={onClick} >
-      {value}
+      {children}
     </button>
   </div>
   );
