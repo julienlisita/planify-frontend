1bddab92a9b6e0a2c84ed8922fea7d6de818b9c2
Julien Lisita <julien.lisita@gmail.com>
2025-01-16 18:22:36 +0100

    fix color and size of Input component

--- src/components/common/Input.jsx
+++ src/components/common/Input.jsx
@@ -6,7 +6,7 @@ const Input = ({placeholder, value, onChange, label, className}) => {
     <input
       id="input-id"
       type="text"
-      className= {`bg-taupe text-linen m-2 py-2 px-4 rounded text-sm ${className}`}
+      className= {`bg-linen border-2 border-taupe placeholder-taupe placeholder-opacity-60 w-96 text-taupe m-2 py-2 px-4 rounded text-sm ${className}`}
       value={value}
       placeholder={placeholder}
       onChange={(e) => onChange(e.target.value)}
